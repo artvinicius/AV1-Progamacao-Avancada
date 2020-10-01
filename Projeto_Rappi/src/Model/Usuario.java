c913ac52396b590314200fdb8ae9e9b78f263369
ArthurViniciusx <arthurviny1999@gmail.com>
2020-10-01 16:54:35 -0300

    Refatore Class Usuario

--- Projeto_Rappi/src/Model/Usuario.java
+++ Projeto_Rappi/src/Model/Usuario.java
@@ -1,104 +1,100 @@
 package Model;
 
 public class Usuario {
-  private String nome;
-  private String ultNome;
-  private String numCelular;
-  private String genero;
-  private int id;
-  private String email;
-  private Classificacao classificacao;
+	private String nome;
+	private String ultNome;
+	private String numCelular;
+	private String genero;
+	private int id;
+	private String email;
+	private Classificacao classificacao;
 
-Usuario(
-    String nome,
-    String ultNome,
-    String numCelular,
-    String genero,
-    int id,
-    String email,
-    Classificacao classificacao
-  ) {
-    this.nome = nome;
-    this.ultNome = ultNome;
-    this.numCelular = numCelular;
-    this.genero = genero;
-    this.id = id;
-    this.email = email;
-    this.classificacao = classificacao;
-  }
+	Usuario(String nome, String ultNome, String numCelular, String genero, int id, String email,
+			Classificacao classificacao) {
+		this.nome = nome;
+		this.ultNome = ultNome;
+		this.numCelular = numCelular;
+		this.genero = genero;
+		this.id = id;
+		this.email = email;
+		this.classificacao = classificacao;
+	}
 
-  Usuario() {}
+	Usuario() {
+	}
 
-  public String getNome() {
-    return nome;
-  }
+	public String getNome() {
+		return nome;
+	}
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-  public String getUltNome() {
-    return ultNome;
-  }
+	public String getUltNome() {
+		return ultNome;
+	}
 
-  public void setUltNome(String ultNome) {
-    this.nome = ultNome;
-  }
+	public void setUltNome(String ultNome) {
+		this.nome = ultNome;
+	}
 
-  public String getNumCelular() {
-    return numCelular;
-  }
+	public String getNumCelular() {
+		return numCelular;
+	}
 
-  public void setNumCelular(String numCelular) {
-    this.numCelular = numCelular;
-  }
+	public void setNumCelular(String numCelular) {
+		this.numCelular = numCelular;
+	}
 
-  public String getGenero() {
-    return genero;
-  }
+	public String getGenero() {
+		return genero;
+	}
 
-  public void setGenero(String genero) {
-    this.genero = genero;
-  }
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 
-  public int getId() {
-    return id;
-  }
+	public int getId() {
+		return id;
+	}
 
-  public void setId(int id) {
-    this.id = id;
-  }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-  public String getEmail() {
-    return email;
-  }
+	public String getEmail() {
+		return email;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-  public Classificacao getClassificacao() {
-    return classificacao;
-  }
+	public Classificacao getClassificacao() {
+		return classificacao;
+	}
 
-  public void setClassificacao(Classificacao classificacao) {
-    this.classificacao = classificacao;
-  }
+	public void setClassificacao(Classificacao classificacao) {
+		this.classificacao = classificacao;
+	}
 
-  public static class Classificacao {
-    private String nivel;
-Classificacao(){
-    
-}
+	public static class Classificacao {
+		private String nivel;
 
-    public String upgrade(){
+		Classificacao() {
 
-        return "SUBINDO"
-    }
-    public String downgrade(){
+		}
 
-        return "DESCENDO"
-    }
+		public String upgrade() {
 
-  }
+			return "SUBINDO";
+		}
+
+		public String downgrade() {
+
+			return "DESCENDO";
+		}
+
+	}
 }
