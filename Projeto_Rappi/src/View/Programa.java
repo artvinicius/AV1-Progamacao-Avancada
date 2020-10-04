d00f77a8093fcf3c816c2e738891ee0d0782b033
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2020-10-04 20:42:11 -0300

    Ajustando a main

--- Projeto_Rappi/src/View/Programa.java
+++ Projeto_Rappi/src/View/Programa.java
@@ -1,8 +1,10 @@
 package View;
 
+import java.io.IOException;
+
 public class Programa {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Operacoes app = new Operacoes();
 
