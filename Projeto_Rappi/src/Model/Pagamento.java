cad9b733fae06cfcc6468047f196a82bacc9e782
LucasEdu10 <lucas.carvalho.luz1@gmail.com>
2020-10-05 01:37:20 -0300

    Adicionando os metodos de acesso do pagamento

--- Projeto_Rappi/src/Model/Pagamento.java
+++ Projeto_Rappi/src/Model/Pagamento.java
@@ -5,6 +5,14 @@ public class Pagamento {
 	private String modo;
 	
 	public Pagamento(String modo) {
+		this.setModo(modo);
+	}
+
+	public String getModo() {
+		return modo;
+	}
+
+	public void setModo(String modo) {
 		this.modo = modo;
 	}
 
